5332e11329f9410c661b0e78c0a6313dbb75028e
sharonlevi94 <sharonlevy96@gmail.com>
2022-02-25 07:44:23 +0200

    fix code

--- SQLTablesCreator.py
+++ SQLTablesCreator.py
@@ -1,6 +1,8 @@
 import psycopg2
 import os
 
+print("First, make sure you have the tables in your PostgreSQL data base with the names of the csv file")
+
 # Get an inputs from the user
 directory = str(input("Please enter a directory path:\n"))
 userName = str(input("Please enter your DB user name:\n"))
@@ -11,8 +13,11 @@ port = str(input("Please enter a port:\n"))
 
 # establishing the connection
 conn = psycopg2.connect(
-    database="mydb", user='postgres', password='123456', host='127.0.0.1', port='5432'
+     database="mydb", user='postgres', password='123456', host='127.0.0.1', port='5432'
 )
+# conn = psycopg2.connect(
+#    database=db, user=userName, password=password, host=host, port=port
+# )
 conn.autocommit = True
 
 # Creating a cursor object using the cursor() method
